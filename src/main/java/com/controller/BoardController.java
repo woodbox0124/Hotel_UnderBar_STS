3198cc36b4c9c97de988a9cc424b99c6378da1d5
Bitna Lee <64954355+blingbn@users.noreply.github.com>
2021-05-18 18:22:15 +0900

    게시판 수정

---
+++ src/main/java/com/controller/BoardController.java
@@ -0,0 +1,87 @@
+package com.controller;
+
+
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.dto.BoardDTO;
+import com.dto.BoardPageDTO;
+import com.dto.MemberDTO;
+import com.service.BoardService;
+import com.service.MemberService;
+
+@Controller
+public class BoardController {
+
+	@Autowired
+	MemberService mService;
+	@Autowired
+	BoardService bService;
+	//시작점
+	
+	//board List
+	@RequestMapping("/loginCheck/boardList")
+	public String boardList(@RequestParam(required=false, defaultValue="1") String curPage ,
+			HttpSession session, @RequestParam(required=false, defaultValue="title") String searchName,
+			@RequestParam(required=false, defaultValue="") String searchValue) throws Exception {
+		System.out.println(curPage);
+		System.out.println(searchName);
+		System.out.println(searchValue);
+	
+		HashMap<String, String> map= new HashMap<String, String>();
+		map.put("searchName", searchName);
+		map.put("searchValue", searchValue);		
+		System.out.println(map);
+		BoardPageDTO pDTO= bService.boardList(Integer.parseInt(curPage),map);	
+		System.out.println(pDTO);
+		session.setAttribute("pDTO", pDTO);
+		
+		return "redirect:../boardList";
+	}
+	//board write
+	@RequestMapping("/loginCheck/boardWrite")
+	public String boardList(HttpSession session) {
+		MemberDTO dto =(MemberDTO)session.getAttribute("login");
+		String u_id = dto.getU_id();
+		MemberDTO mdto = mService.myPage(u_id);
+		System.out.println(mdto);
+		session.setAttribute("login", mdto);//다시 session에 저장
+		return "redirect:../boardWrite";
+	}
+	
+	//board write Insert
+	@RequestMapping(value="/loginCheck/boardInsert", produces="text/plain;charset=UTF-8")
+	public String boardInsert(BoardDTO bDTO, HttpSession session) {
+		System.out.println(bDTO);
+		bDTO.setAuthor(bDTO.getAuthor());
+		bDTO.setTitle(bDTO.getTitle());
+		bDTO.setContent(bDTO.getContent());
+		
+		int n = bService.boardInsert(bDTO);
+		System.out.println(n);
+		return "redirect:../loginCheck/boardList";
+	}
+	//board write 불러오기 
+	@RequestMapping(value="/loginCheck/boardRetrive", produces="text/plain;charset=UTF-8")
+	public String boardRetrieve(@RequestParam int num, HttpSession session) {
+		
+		BoardDTO bDTO = bService.selectByNum(num);
+		System.out.println(bDTO);
+		
+		
+		
+		
+		
+		
+		return "redirect:../loginCheck/boardList";
+	}
+}
--- src/main/webapp/WEB-INF/views/board/list.jsp
+++ src/main/webapp/WEB-INF/views/board/list.jsp
@@ -57,7 +57,9 @@ margin-bottom: 10px;
 {
 	text-align: center;
 }
-
+.container{
+	height:790px;
+}
 #texttitle{
 text-overflow: ellipsis; white-space: nowrap; max-width: 40px; /* 40px를 넘어가는 제목일 경우 "제목..."으로 표기됨 */
 }
@@ -123,21 +125,14 @@ for (BoardDTO dto : list) {
 	int origin=dto.getOrigin();
 	int groupnum=dto.getGroupnum();
 	int grouplayer=dto.getGrouplayer();
-
-
 	
 %>
-
-			
 				
 	<tbody>
-			
-				
 	<tr>
 		<td class="normal"><%=num %></td>
 		<td align="left">
 <% 
-		
 	for(int j=0;j<grouplayer;j++){
 %>
 		&nbsp;&nbsp;&nbsp;
@@ -150,7 +145,7 @@ for (BoardDTO dto : list) {
 <%
 	}
 %> 
-		<a class="tablename" href="BoardRetrive?num=<%=num%>"><%=title %></a> </td>
+		<a class="tablename" href="boardRetrive?num=<%=num%>"><%=title %></a> </td>
 		<td class="normal"><%=author %></td>
 		<td class="normal"><%=writeday %></td>
 		<td class="normal"><%=readcnt %></td>
@@ -182,7 +177,6 @@ out.print("<a href='boardList?curPage="+i+"&searchName="+searchName+"&searchValu
 <a class="btn btn-primary" href="loginCheck/boardWrite">글쓰기</a>	
 </div>
 
-	
 </div>
 </body>
 </html>
