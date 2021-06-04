5d175b0f8f50aedb1080ffa952acb969ed5e3ede
ohysad <ohyasd@naver.com>
2021-06-04 17:53:23 +0900

    리뷰 count

--- src/main/java/com/dto/ReviewCountDTO.java
+++ src/main/java/com/dto/ReviewCountDTO.java
@@ -6,18 +6,20 @@ import org.apache.ibatis.type.Alias;
 public class ReviewCountDTO {
 int numrating;
 double count;
+int gc;
 public ReviewCountDTO() {
 	super();
 	// TODO Auto-generated constructor stub
 }
-public ReviewCountDTO(int numrating, double count) {
+public ReviewCountDTO(int numrating, double count, int gc) {
 	super();
 	this.numrating = numrating;
 	this.count = count;
+	this.gc = gc;
 }
 @Override
 public String toString() {
-	return "ReviewCountDTO [numrating=" + numrating + ", count=" + count + "]";
+	return "ReviewCountDTO [numrating=" + numrating + ", count=" + count + ", gc=" + gc + "]";
 }
 public int getNumrating() {
 	return numrating;
@@ -31,6 +33,13 @@ public double getCount() {
 public void setCount(double count) {
 	this.count = count;
 }
+public int getGc() {
+	return gc;
+}
+public void setGc(int gc) {
+	this.gc = gc;
+}
+
 
 
 }
